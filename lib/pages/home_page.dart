3b3e0a12c13f3bed6a52dfc9744ddedee9a27fc9
Poul <anrivandloup@gmail.com>
2022-12-02 16:15:50 -0500

    cuarto video

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,27 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class HomePage extends StatelessWidget {
+ 
+ List pokemons =[];
   getDataPokemon() async {
     Uri _uri = Uri.parse(
         "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json");
     http.Response response = await http.get(_uri);
-    print(response.statusCode);
-    print(response.body);
+   if(response.statusCode== 200){
+    Map<String, dynamic> myMap = json.decode(response.body);
+    pokemons = myMap["pokemon"];
+    print(pokemons);
+   }
+
   }
 
   @override
   Widget build(BuildContext context) {
+
+    getDataPokemon();
     return Scaffold(
         body: SafeArea(
       child: SingleChildScrollView(
